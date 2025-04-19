f454acb62129dcb67faf04e64bab0e5aef3c7352
Fraydaliz Carpio <carpiofraydaliz19@gmail.com>
2025-04-18 22:38:18 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import  _  from "lodash"
+
+const mary = "mary"
+
+
+
+const upperMary= _.upperFirst(mary)
+
+console.log(upperMary)
+
+
+
+
+
+
+import {addNums, greet} from "./utils.js"
+
+console.log(greet("Mary"))
+
+
+addNums
+
+console.log(5, 6)
+
+
---
+++ utils.js
@@ -0,0 +1,10 @@
+'use strict'
+
+export function greet(name){
+    return `hello! + ${name}`
+
+}
+
+export function addNums(a , b){
+return a + b
+}
